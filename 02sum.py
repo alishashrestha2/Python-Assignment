5c401caf580320e1b8ebfa55e400fde0f0779e2f
Alisha Shrestha <shresthaalisha249@gmail.com>
2024-06-08 07:43:18 +0545

    sum of two numbers

---
+++ 02sum.py
@@ -0,0 +1,5 @@
+#A program to find sum of two numbers. Use input() function to take input from the user.
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+sum = num1 + num2
+print(f"\n{num1} + {num2} = {sum}")
