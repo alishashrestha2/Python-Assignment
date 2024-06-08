961cb72abdf2e245a676a8b2b5d38ec13f0e8160
Alisha Shrestha <shresthaalisha249@gmail.com>
2024-06-08 07:45:18 +0545

    discount

---
+++ 04discount.py
@@ -0,0 +1,27 @@
+"""A program to calculate discount on the basis of following assumption:
+a) If purchased amount is greater than or equal to 1000, discount is 5%
+b) If purchased amount is less than 1000, discount is 3%"""
+amount = float(input("Enter the purchased amount : "))
+discount_percentage = 0 
+discount_amount = 0
+final_amount = 0
+if amount >= 1000 :
+    discount_percentage = 0.05
+elif amount < 1000 and amount > 0 :
+    discount_percentage = 0.03
+else :
+    print("Error")
+discount_amount = discount_percentage * amount
+final_amount = amount - discount_amount
+print("\n\n-------------------Bill-------------------")
+print("________________________________________")
+print("|Particulars\t\t|Details\t|")
+print("|-----------------------|---------------|")
+print(f"|Total Amount\t\t|Rs.{amount}\t|")
+print("|-----------------------|---------------|")
+print(f"|Discount rate\t\t|{discount_percentage * 100}%\t\t|")
+print("|-----------------------|---------------|")
+print(f"|Discount Amount\t|Rs.{discount_amount}\t|")
+print("|-----------------------|---------------|")
+print(f"|Final Amount\t\t|Rs.{final_amount}\t|")
+print("|_______________________|_______________|\n\n")
