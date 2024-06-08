78c5a44a844a951166758962accfc153d31ea01a
Alisha Shrestha <shresthaalisha249@gmail.com>
2024-06-08 07:46:39 +0545

    odd and even number

---
+++ 07oddeven.py
@@ -0,0 +1,6 @@
+#A program to test whether a number is even or odd.
+number = int(input("Enter a number : "))
+if number % 2 == 0 :
+    print(f"The number {number} is even.")
+else :
+    print(f"The number {number} is odd.")
